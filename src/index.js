bff8d3eee4c6cdb93c35a2e60fe03712dff2a9fb
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-04 20:16:11 -0300

    Configuração Reactotron

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,7 +19,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>React Native</Text>
+      <Text style={styles.welcome}>Reacasdt Native</Text>
     </View>
   );
 }

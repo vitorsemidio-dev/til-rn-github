f88009dd7bb6a67b46d4b5884ff06fa2c2ebefd5
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-05 19:41:39 -0300

    Navegação entre rotas

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Reacasdt Native</Text>
-    </View>
-  );
+  return <Routes />;
 }

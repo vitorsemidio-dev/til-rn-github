93399e765b844d2d71959892158afe3323380975
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-06 21:39:11 -0300

    Instalação styled-component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
   return (
-    <View>
+    <Container>
       <Text>Main</Text>
-    </View>
+    </Container>
   );
 }

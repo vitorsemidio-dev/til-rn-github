1d3ff3cc2fbb7ca27ade9e6d1d65217d4cf977a5
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-09 19:13:05 -0300

    PropTypes User

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
 import api from '../../services/api';
 
 import { Container } from './styles';
 
 export default class User extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      setOptions: PropTypes.func,
+    }).isRequired,
+    route: PropTypes.shape({
+      params: PropTypes.shape({
+        user: PropTypes.object
+      })
+    }).isRequired
+  }
 
   state = {
     repos: [],
@@ -12,7 +23,7 @@ export default class User extends Component {
 
   async componentDidMount() {
     this.navigationOptions(this.props);
-    const { navigation, route } = this.props;
+    const { route } = this.props;
     const { user } = route.params;
 
     const response = await api.get(`/users/${user.login}/repos`);
@@ -27,6 +38,7 @@ export default class User extends Component {
   }
 
   render() {
+    const { repos } = this.state;
     return (
       <Container>
         <Text>User</Text>

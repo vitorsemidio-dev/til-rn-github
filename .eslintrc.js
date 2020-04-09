6a45c1d9f50e4fb2905e1606ff5abb2fc79c1f04
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-09 01:27:10 -0300

    Buscando dados do usuário no github

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,5 +35,6 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'react/prefer-stateless-function': 'off',
+    'react/state-in-constructor': 'off'
   },
 };
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import api from '../../services/api';
 import { Container, Form, Input, SubmitButton } from './styles';
 
 Icon.loadFont();
 
 export default class Main extends Component {
+  state = {
+    users: [],
+    newUser: '',
+  };
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
   render() {
+    const { users, newUser } = this.state;
     return (
       <Container>
         <Form>
@@ -14,9 +42,13 @@ export default class Main extends Component {
             autoCorret={false}
             autoCapitalize="none"
             placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={(text) => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
           />
 
-          <SubmitButton>
+          <SubmitButton onPress={this.handleAddUser}>
             <Icon name="add" size={20} color="#FFF" />
           </SubmitButton>
         </Form>

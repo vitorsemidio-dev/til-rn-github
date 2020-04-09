6a9b87718c9d7fe37dde8c6b138a113333fa3a7a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-09 18:39:18 -0300

    Configurando titulo componente usuário

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import { Container } from './styles';
 
-export default function User({ route }) {
-  console.tron.log(route.params.user);
-  return (
-    <Container>
-      <Text>User</Text>
-    </Container>
-  );
+export default class User extends Component {
+
+  componentDidMount() {
+    this.navigationOptions(this.props);
+  }
+
+  navigationOptions({ navigation, route }) {
+    const { user } = route.params
+    navigation.setOptions({ title: user.name });
+    console.tron.log(this.props);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Text>User</Text>
+      </Container>
+    );
+  }
 }
